de20656fd67ad592c94e8fa1c4cb13bc83820eb4
haz0110 <hazar@a0110.com>
2023-05-28 10:26:08 +0300

    optimizations

--- src/problem1.rs
+++ src/problem1.rs
@@ -1,8 +1,9 @@
 use hazs_tools::mathematics::common::merge_two_arrays_arrange_and_clean;
-use std::iter::Sum;
 
 pub fn problem1() -> usize {
-    let combined_array: Vec<usize> =
-        merge_two_arrays_arrange_and_clean(&mut vec![3; (1000 / 3)], &mut vec![5; (1000 / 5)]);
-    Sum::sum(combined_array.iter())
+
+    let mut array1: Vec<usize> = vec![3; 1000/3].into_iter().map(|x| x + 3).rev().collect();
+    let mut array2: Vec<usize> = vec![3; 1000/3];
+
+    merge_two_arrays_arrange_and_clean(&mut array1, &mut array2).iter().sum()
 }
